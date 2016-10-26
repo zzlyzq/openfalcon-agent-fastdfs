4a7d4188375ace4ede0662593b9cc9784a067b90
Jacky <zzlyzq@126.com>
2016-10-26 09:29:19 +0800

    优化

--- fdfs_check.py
+++ fdfs_check.py
@@ -12,21 +12,28 @@ import time
 import ast
 import yaml
 
+# 定义falcon上报数据的时候用到的变量
 falconTs = int(time.time())
 falconEndpoint = "cluster-fastdfs"
 falconTimeStamp = 60
+falconPayload=[]
 
+# 对于monitor监控到的数据，用以下的变量去存放采集到的数据
 serverinfo={}
 currentGroupNumber=""
 currentStorageNumber=""
 
-falconPayload=[]
-
-
 # 执行命令
-cmdLine = "/home/123/opt/fastdfs/usr/bin/fdfs_monitor /home/123/opt/fastdfs/etc/fdfs/client.conf"
+cmdLine = "/home/machtalk/opt/fastdfs/usr/bin/fdfs_monitor /home/machtalk/opt/fastdfs/etc/fdfs/client.conf"
 cmdResult = os.popen(cmdLine).readlines()
 
+# 定义函数，由于采集到的数据有好几种格式
+# 100， 数值
+# 5.05， 版本号
+# 2016-10-10 10:10:10， 时间格式
+# 10.10.10.10， ip地址格式
+# ACTIVE， 存货代表1，其他使用0
+# 该函数会把这些转换为float类型或者整形。
 def falconValue(value):
     result = re.findall("(\d+\-\d+\-\d+ \d+\:\d+\:\d+)",value)
     if len(result) != 0:
@@ -59,14 +66,17 @@ def falconValue(value):
         print "异常"
         return -1
 
+# 默认采用GAUGE的形式，如果有COUNTER类型，尤其是时间类型，那么就加入下面的列表
 def falconType(value):
     counterTypeList = []
     counterTypeList.append("up time")
     counterTypeList.append("join time")
+    counterTypeList.append("last_heart_beat_time")
     if value in counterTypeList:
         return "COUNTER"
     return "GAUGE"
 
+# 根据之前的cmdline执行结果，进行处理
 for line in cmdResult:
     # check server_count 和 server_index
     result1=re.findall("server_count=(\d+), server_index=(\w+)",line)
@@ -108,7 +118,7 @@ for line in cmdResult:
         print result16
         currentStorageNumber = result16[0]
         serverinfo[currentGroupNumber][currentStorageNumber]={}
-        print "遇到了新的Storage:%s"%(currentStorageNumber)
+        #print "遇到了新的Storage:%s"%(currentStorageNumber)
         continue
     # 使用列表去处理
     storage_item_list=["id","ip_addr","http domain","version","join time","up time","total storage","free storage","upload priority","store_path_count","subdir_count_per_path","storage_port","storage_http_port","current_write_path","source","if_trunk_server","connection.alloc_count","connection.current_count","connection.max_count","total_upload_count","success_upload_count","total_append_count","success_append_count","total_modify_count","success_modify_count","total_truncate_count","success_truncate_count","total_set_meta_count","success_set_meta_count","total_delete_count","success_delete_count","total_download_count","success_download_count","total_get_meta_count","success_get_meta_count","total_create_link_count","success_create_link_count","total_delete_link_count","success_delete_link_count","total_upload_bytes","success_upload_bytes","total_append_bytes","success_append_bytes","total_modify_bytes","success_modify_bytes","stotal_download_bytes","success_download_bytes","total_sync_in_bytes","success_sync_in_bytes","total_sync_out_bytes","success_sync_out_bytes","total_file_open_count","success_file_open_count","total_file_read_count","success_file_read_count","total_file_write_count","success_file_write_count","last_heart_beat_time","last_source_update","last_sync_update","last_synced_timestamp", "connection.alloc_count","connection.current_count","connection.max_count","total_upload_count","success_upload_count","total_append_count","success_append_count","total_modify_count","success_modify_count","total_truncate_count","success_truncate_count","total_set_meta_count","success_set_meta_count","total_delete_count","success_delete_count","total_download_count","success_download_count","total_get_meta_count","success_get_meta_count","total_create_link_count","success_create_link_count","total_delete_link_count","success_delete_link_count","total_upload_bytes","success_upload_bytes","total_append_bytes","success_append_bytes","total_modify_bytes","success_modify_bytes","stotal_download_bytes","success_download_bytes","total_sync_in_bytes","success_sync_in_bytes","total_sync_out_bytes","success_sync_out_bytes","total_file_open_count","success_file_open_count","total_file_read_count","success_file_read_count","total_file_write_count","success_file_write_count","last_heart_beat_time","last_source_update","last_sync_update","last_synced_timestamp"]
@@ -116,23 +126,24 @@ for line in cmdResult:
         print "开始寻找"+storage_item
         result = re.findall("^\s+%s = ([\S ]+)"%storage_item,line)
         if len(result) == 1:
-	    print "发现匹配",
-            print line
-            print result
+	    #print "发现匹配",
+            #print line
+            #print result
             serverinfo[currentGroupNumber][currentStorageNumber][storage_item] = result[0]
             payloadString="""{ "endpoint": "%s", "metric": "%s", "timestamp": %s, "step": %s, "value": %s, "counterType": "%s", "tags": "%s"} """%(falconEndpoint, storage_item, falconTs, falconTimeStamp, falconValue(serverinfo[currentGroupNumber][currentStorageNumber][storage_item]),falconType(storage_item),"group="+currentGroupNumber+",storage="+currentStorageNumber)
             falconPayload.append(yaml.load(payloadString))
             break
 
+# 以下主要用于打印测试，pprint这个不错，可以格式化列表或者字典
 #print serverinfo
 #print len(serverinfo)
-pp = pprint.PrettyPrinter(indent = 4)
-pp.pprint(serverinfo)
+#pp = pprint.PrettyPrinter(indent = 4)
+#pp.pprint(serverinfo)
 #pp.pprint(falconPayload)
+#print type(falconPayload)
+#print len(falconPayload)
+#print type(falconPayload[0])
+#print json.dumps(falconPayload)
 
-print type(falconPayload)
-print len(falconPayload)
-print type(falconPayload[0])
-print json.dumps(falconPayload)
 r = requests.post("http://192.168.3.228:1988/v1/push", data=json.dumps(falconPayload))
 print r.text
